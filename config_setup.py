de29b6f23a80942ad772dd950d9746cf238ef439
hrdkmishra <hardikmishra0509@gmail.com>
2023-07-22 23:35:56 +0530

    config_setup.py clean up

--- config_setup.py
+++ config_setup.py
@@ -16,13 +16,3 @@ def load_credentials_from_config():
             config_data = toml.load(config_file)
         return config_data.get("LEETCODE_SESSION"), config_data.get("CSRF_TOKEN")
     return None, None
-
-
-def create_python_code_edit_file(question_id, title_slug, python3_code):
-    if python3_code is None:
-        print(f"No Python3 code snippet found for question '{question_id}'.")
-        return
-
-    file_name = f"code_editor/{question_id}_{title_slug}.py"
-    with open(file_name, "w") as file:
-        file.write(python3_code.code)
