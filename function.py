72cd8c9bbe450bd0d8c5cf1593ea98c7ac6adcd2
hrdkmishra <hardikmishra0509@gmail.com>
2023-07-22 23:21:39 +0530

    without leetcode library

--- function.py
+++ function.py
@@ -1,21 +1,7 @@
 import json
-import leetcode
 from bs4 import BeautifulSoup
 
 from color import Colors
-
-
-# configuration
-def configure_api_instance(csrf_token, leetcode_session):
-    configuration = leetcode.Configuration()
-    configuration.api_key["x-csrftoken"] = csrf_token
-    configuration.api_key["csrftoken"] = csrf_token
-    configuration.api_key["LEETCODE_SESSION"] = leetcode_session
-    configuration.api_key["Referer"] = "https://leetcode.com"
-    configuration.debug = False
-
-    api_instance = leetcode.DefaultApi(leetcode.ApiClient(configuration))
-    return api_instance
 
 
 def process_api_response(api_response):
@@ -30,54 +16,14 @@ def read_json_file(file_path):
         return json.load(json_file)
 
 
-# for get question data by question id
-def get_question_data_by_id(json_data, q):
-    questions = (
-        json_data.get("data", {}).get("problemsetQuestionList", {}).get("questions", [])
-    )
-
-    for question in questions:
-        if q == question.get("frontendQuestionId"):
-            return question
-    return None
-
-
-# for graphql excution
-def execute_graphql_query(api_instance, graphql_query, query_variables):
-    graphql_request = leetcode.GraphqlQuery(query=graphql_query, variables=query_variables)
-    api_response = api_instance.graphql_post(body=graphql_request)
-    return api_response
+# # for graphql excution
+# def execute_graphql_query(api_instance, graphql_query, query_variables):
+#     graphql_request = leetcode.GraphqlQuery(query=graphql_query, variables=query_variables)
+#     api_response = api_instance.graphql_post(body=graphql_request)
+#     return api_response
 
 
 # this is for print the question list in order
-def print_question_data(question):
-    question_id = question.get("frontendQuestionId")
-    title = question.get("title")
-    difficulty = question.get("difficulty")
-    ac_rate = question.get("acRate")
-
-    # Fix ac_rate position regardless of the length of difficulty
-    difficulty_color = ""
-    if difficulty == "Easy":
-        difficulty_color = Colors.GREEN
-    elif difficulty == "Medium":
-        difficulty_color = Colors.ORANGE
-    elif difficulty == "Hard":
-        difficulty_color = Colors.RED
-
-    # Set fixed widths for the title and difficulty columns
-    title_width = 50
-    difficulty_width = 10
-
-    # Align and pad the title and difficulty columns
-    title_formatted = title.ljust(title_width)[:title_width]
-    difficulty_formatted = (
-        f"{difficulty_color}{difficulty.ljust(difficulty_width)}{Colors.RESET}"
-    )
-
-    print(
-        f"[{str(question_id).rjust(3)}]  {title_formatted}  {difficulty_formatted}  ({ac_rate:.2f}%)"
-    )
 
 
 # for --solve
@@ -130,9 +76,9 @@ def fetch_code_content(api_instance, title_slug):
     #             return python3_snippet
 
     python3_snippet = next(
-                (snippet for snippet in api_response.data.question.code_snippets if snippet.lang_slug == "python3"),
-                None
-            )
+        (snippet for snippet in api_response.data.question.code_snippets if snippet.lang_slug == "python3"),
+        None
+    )
     if python3_snippet:
         return python3_snippet
 
