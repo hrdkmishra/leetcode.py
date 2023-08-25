f2596c57a63c14b5979e47aae63e4ee19d60d5e4
hardik <87580981+hrdkmishra@users.noreply.github.com>
2023-08-25 12:43:25 +0530

    Update config_setup.py
    optimized

--- config_setup.py
+++ config_setup.py
@@ -4,34 +4,52 @@ import os
 CONFIG_FILE_PATH = "config.toml"
 
 
-def save_credentials_to_config(leetcode_session, csrf_token):
-    config_data = {"LEETCODE_SESSION": leetcode_session, "CSRF_TOKEN": csrf_token}
+def update_config(key_value_dict):
+    if os.path.exists(CONFIG_FILE_PATH):
+        with open(CONFIG_FILE_PATH, "r") as config_file:
+            config_data = toml.load(config_file)
+        config_data.update(key_value_dict)
+    else:
+        config_data = key_value_dict
+
     with open(CONFIG_FILE_PATH, "w") as config_file:
         toml.dump(config_data, config_file)
 
 
-def load_credentials_from_config():
+def load_config_from_file():
     if os.path.exists(CONFIG_FILE_PATH):
         with open(CONFIG_FILE_PATH, "r") as config_file:
-            config_data = toml.load(config_file)
-        return config_data.get("LEETCODE_SESSION"), config_data.get("CSRF_TOKEN")
-    return None, None
+            return toml.load(config_file)
+    return {}
+
+
+def save_credentials_to_config(leetcode_session, csrf_token):
+    config_data = {
+        "LEETCODE_SESSION": leetcode_session,
+        "CSRF_TOKEN": csrf_token
+    }
+    update_config(config_data)
+
+
+def load_credentials_from_config():
+    config_data = load_config_from_file()
+    return config_data.get("LEETCODE_SESSION"), config_data.get("CSRF_TOKEN")
 
 
 def load_user_data_from_config():
-    if os.path.exists(CONFIG_FILE_PATH):
-        with open(CONFIG_FILE_PATH, "r") as config_file:
-            config_data = toml.load(config_file)
-        return config_data.get("USER_LANG", "").lower() , config_data.get("EDITOR_CLI", "").lower()
-    return None
+    config_data = load_config_from_file()
+    return config_data.get("USER_LANG", "").lower(), config_data.get("EDITOR_CLI", "").lower()
 
 
 def save_user_data_to_config(user_lang):
     config_data = {"USER_LANG": user_lang}
-    if os.path.exists(CONFIG_FILE_PATH):
-        with open(CONFIG_FILE_PATH, "r") as config_file:
-            existing_config_data = toml.load(config_file)
-        existing_config_data.update(config_data)
-        config_data = existing_config_data
-    with open(CONFIG_FILE_PATH, "w") as config_file:
-        toml.dump(config_data, config_file)
+    update_config(config_data)
+
+
+def save_user_path_to_config(path):
+    config_data = {"LEETCODE_PATH": path}
+    update_config(config_data)
+    
+def load_user_path_from_config():
+    config_data = load_config_from_file()
+    return config_data.get("LEETCODE_PATH", "")
